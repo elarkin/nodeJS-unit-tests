252361bd6612795a8a324e5881c80f860078a3bf
Evan Larkin <evan.larkin.iit@gmail.com>
2010-09-11 18:18:27 -0500

    Grouped the reports by sub-test instead of by type

--- src/test.js
+++ src/test.js
@@ -3,8 +3,7 @@ var print = require('sys').print;
 var puts = require('sys').puts;
 var inspect = require('sys').inspect;
 
-var tests_with_errors = [];
-var tests_with_failures = [];
+var failures = [];
 var suites = [];
 
 var exec_test = function(name, func, suite) {
@@ -14,69 +13,66 @@ var exec_test = function(name, func, suite) {
 		func.call(suite);
 		print('.'); // success!
 	} catch(err) {
-		if(err.name !== 'AssertionError') {
-			tests_with_errors.push({
-				"name": name,
-				"err": err
-			});
+		var is_error = err.name !== 'AssertionError';
+		failures.push({
+			"name": name,
+			"err": err,
+			"is_error": is_error
+		});
+
+		if(is_error) {
 			print('E');
 		} else {
-			tests_with_failures.push({
-				"name": name,
-				"err": err
-			});
 			print('F');
 		}
 	}
 };
 
 var print_details = function() {
-	if(tests_with_errors.length === 0 && tests_with_failures.length === 0){
+	if(failures.length === 0){
 		return; //nothing to print
 	}
 
 	puts('');
-	puts('');
 	var test;
-	var len = tests_with_errors.length;
+	var len = failures.length;
 	while(len--) {
-		test = tests_with_errors.pop();
-		print("EXCEPTION: ");
-		puts(test.name);
+		test = failures.shift();
 		puts('');
-		puts(inspect(test.err));
-		puts('');
-	}
+		if(test.is_error){
+			print("EXCEPTION: ");
+			puts(test.name);
+			puts('');
+			puts(inspect(test.err));
+			puts('');
+		} else {
+			print("FAILURE: ");
+			puts(test.name);
+			puts('');
 
-	len = tests_with_failures.length;
-	while(len--) {
-		test = tests_with_failures.pop();
-		print("FAILURE: ");
-		puts(test.name);
-		puts('');
-
-		var err = test.err;
+			var err = test.err;
 		
-		if(err.message) {
-			print("Message: ");
-			puts(err.message);
-		}
+			if(err.message) {
+				print("Message: ");
+				puts(err.message);
+			}
 
-		if(err.actual !== undefined || err.expected !== undefined) {
-			print("Actual: ");
-			puts(inspect(err.actual));
-			print("Expected: ");
-			puts(inspect(err.expected));
-		}
+			if(err.actual !== undefined || err.expected !== undefined) {
+				print("Actual: ");
+				puts(inspect(err.actual));
+				print("Expected: ");
+				puts(inspect(err.expected));
+			}
 
-		if(err.operator) {
-			print("Operator: ");
-			puts(inspect(err.operator));
-		}
+			if(err.operator) {
+				print("Operator: ");
+				puts(inspect(err.operator));
+			}
 
-		if(err.stack !== undefined) {
-			puts("Stacktrace:");
-			puts(err.stack);
+			if(err.stack !== undefined) {
+				puts("Stacktrace:");
+				puts(err.stack);
+			}
 		}
 	}
 };
