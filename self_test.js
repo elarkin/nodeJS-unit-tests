5db877c9c163e19a7809241f416a3e4e46b5bafd
Evan Larkin <evan.larkin.iit@gmail.com>
2010-09-11 19:35:31 -0500

    Made the example run show the depth first nature of subtests more obviously.

--- self_test.js
+++ self_test.js
@@ -5,26 +5,30 @@ var test = require('test');
 
 var suite, subtest
 suite = {};
-subtest = {};
+subtest1 = {};
+subtest2 = {};
+subtest1_subtest = {};
 
-subtest["test"] = suite["test"] = function() {
+suite["test"] = function() {
 	//I should not execute
 };
 
-subtest["test pass"] = suite["test pass"] = function() {
+suite["test pass"] = function() {
 	//pass;
 };
 
-subtest["test error"] = suite["test error"] = function() {
+suite["test error"] = function() {
 	throw {
 		message: "This is an example of just some random error"
 	};
 };
 
-subtest["test assert failure"] = suite["test assert failure"] = function() {
+subtest1_subtest["test assert failure"] = subtest2["test assert failure"] = subtest1["test assert failure"] = suite["test assert failure"] = function() {
 	assert.ok(false,"Normal Assertion Failure");
 };
 
-suite["test subtest"] = subtest
+suite["test branch1"] = subtest1;
+subtest2["test branch2 subtests"] = subtest1["test branch1 subtest"] = subtest1_subtest;
+suite["test branch2"] = subtest2;
 
 test.run(suite);
